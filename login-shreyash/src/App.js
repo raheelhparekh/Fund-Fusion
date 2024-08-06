4839106c9e483d3e88a1eff3af70697de344ea9b
Shreyashp714 <shreyashp714@gmail.com>
2024-08-06 15:12:17 +0530

    Updated navbar box shadow and footer

--- login-shreyash/src/App.js
+++ login-shreyash/src/App.js
@@ -4,14 +4,17 @@ import Login from './pages/Login';
 import Navbar from './components/Navbar/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthProvider } from './contexts/UserContext';
+import Footer from './components/Footer.js/Footer';
 
 
 function App() {
   return (
     <AuthProvider>
+
     <div className="App">
       <Navbar />
       <Login />
+      <Footer />
     </div>
     </AuthProvider>
   );
--- login-shreyash/src/components/Navbar/Navbar.js
+++ login-shreyash/src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Navbar.css'
 
 const Navbar = () => {
   return (
@@ -9,8 +10,8 @@ const Navbar = () => {
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ml-auto">
+      <div className="collapse navbar-collapse navbar-items" id="navbarNav">
+        <ul className="navbar-nav ml-auto align-center">
           <li className="nav-item">
             <a className="nav-link active" href="#">Dashboard</a>
           </li>
